62c3a91f8cdc624ba2846d520f2570075c1f1a3a
zouhangte <zouhangtezbm@126.com>
2017-02-21 19:13:11 +0800

    纯色块支持按扭

--- Assets/PSD2UGUI/Editor/LayerImport/ButtonLayerImport.cs
+++ Assets/PSD2UGUI/Editor/LayerImport/ButtonLayerImport.cs
@@ -18,55 +18,22 @@ namespace PSDUIImporter
 
         public void DrawLayer(Layer layer, GameObject parent)
         {
-            UnityEngine.UI.Button button = PSDImportUtility.InstantiateItem<UnityEngine.UI.Button>(PSDImporterConst.PREFAB_PATH_BUTTON, layer.name,parent);
+            UnityEngine.UI.Button button = PSDImportUtility.InstantiateItem<UnityEngine.UI.Button>(PSDImporterConst.PREFAB_PATH_BUTTON, layer.name, parent);
 
             if (layer.images != null)
             {
                 for (int imageIndex = 0; imageIndex < layer.images.Length; imageIndex++)
                 {
                     Image image = layer.images[imageIndex];
-                    string lowerName = image.name.ToLower();
                     if (image.imageType == ImageType.Image && image.name.ToLower().Contains("normal") || image.name.ToLower().Contains("pressed") || image.name.ToLower().Contains("disabled") || image.name.ToLower().Contains("highlighted"))
                     {
-                        if (image.imageSource == ImageSource.Custom || image.imageSource == ImageSource.Normal)
+                        if (image.arguments == null || image.arguments.Length == 0)
                         {
-                            string assetPath = PSDImportUtility.baseDirectory + image.name + PSDImporterConst.PNG_SUFFIX;
-                            Sprite sprite = AssetDatabase.LoadAssetAtPath(assetPath, typeof(Sprite)) as Sprite;
-
-                            if (lowerName.Contains("normal"))
-                            {
-                                button.image.sprite = sprite;
-
-                                RectTransform rectTransform = button.GetComponent<RectTransform>();
-                                rectTransform.sizeDelta = new Vector2(image.size.width, image.size.height);
-                                rectTransform.anchoredPosition = new Vector2(image.position.x, image.position.y);
-
-                                //rectTransform.SetParent(parent.transform, true);
-
-                                //PosLoader posloader = button.gameObject.AddComponent<PosLoader>();
-                                //posloader.worldPos = rectTransform.position;
-                            }
-                            else if (lowerName.Contains("pressed"))
-                            {
-                                button.transition = UnityEngine.UI.Selectable.Transition.SpriteSwap;
-                                UnityEngine.UI.SpriteState state = button.spriteState;
-                                state.pressedSprite = sprite;
-                                button.spriteState = state;
-                            }
-                            else if (lowerName.Contains("disabled"))
-                            {
-                                button.transition = UnityEngine.UI.Selectable.Transition.SpriteSwap;
-                                UnityEngine.UI.SpriteState state = button.spriteState;
-                                state.disabledSprite = sprite;
-                                button.spriteState = state;
-                            }
-                            else if (lowerName.Contains("highlighted"))
-                            {
-                                button.transition = UnityEngine.UI.Selectable.Transition.SpriteSwap;
-                                UnityEngine.UI.SpriteState state = button.spriteState;
-                                state.highlightedSprite = sprite;
-                                button.spriteState = state;
-                            }
+                            SetSpriteSwipe(image, button);
+                        }
+                        else
+                        {
+                            SetColorSwipe(image, button);
                         }
                     }
                     else
@@ -75,19 +42,84 @@ namespace PSDUIImporter
                     }
                 }
             }
-
         }
-    }
-
-
-    [ExecuteInEditMode]
-    public class PosLoader : MonoBehaviour
-    {
-        public Vector2 worldPos;
-        void Start()
+        private void SetSpriteSwipe(Image image,UnityEngine.UI.Button button)
         {
-            transform.position = worldPos;
-            DestroyImmediate(this);
+            string lowerName = image.name.ToLower();
+            string assetPath = PSDImportUtility.baseDirectory + image.name + PSDImporterConst.PNG_SUFFIX;
+            Sprite sprite = AssetDatabase.LoadAssetAtPath(assetPath, typeof(Sprite)) as Sprite;
+
+            if (lowerName.Contains("normal"))
+            {
+                button.image.sprite = sprite;
+
+                RectTransform rectTransform = button.GetComponent<RectTransform>();
+                rectTransform.sizeDelta = new Vector2(image.size.width, image.size.height);
+                rectTransform.anchoredPosition = new Vector2(image.position.x, image.position.y);
+            }
+            else if (lowerName.Contains("pressed"))
+            {
+                button.transition = UnityEngine.UI.Selectable.Transition.SpriteSwap;
+                UnityEngine.UI.SpriteState state = button.spriteState;
+                state.pressedSprite = sprite;
+                button.spriteState = state;
+            }
+            else if (lowerName.Contains("disabled"))
+            {
+                button.transition = UnityEngine.UI.Selectable.Transition.SpriteSwap;
+                UnityEngine.UI.SpriteState state = button.spriteState;
+                state.disabledSprite = sprite;
+                button.spriteState = state;
+            }
+            else if (lowerName.Contains("highlighted"))
+            {
+                button.transition = UnityEngine.UI.Selectable.Transition.SpriteSwap;
+                UnityEngine.UI.SpriteState state = button.spriteState;
+                state.highlightedSprite = sprite;
+                button.spriteState = state;
+            }
+        }
+
+        private void SetColorSwipe(Image image, UnityEngine.UI.Button button)
+        {
+            string lowerName = image.name.ToLower();
+            Color color;
+            if (ColorUtility.TryParseHtmlString(image.arguments[0], out color))
+            {
+                if (lowerName.Contains("normal"))
+                {
+                    RectTransform rectTransform = button.GetComponent<RectTransform>();
+                    rectTransform.sizeDelta = new Vector2(image.size.width, image.size.height);
+                    rectTransform.anchoredPosition = new Vector2(image.position.x, image.position.y);
+
+                    button.image.color = color;
+                    button.transition = UnityEngine.UI.Selectable.Transition.ColorTint;
+                    UnityEngine.UI.ColorBlock state = button.colors;
+                    state.normalColor = color;
+                    button.colors = state;
+                }
+                else if (lowerName.Contains("pressed"))
+                {
+                    button.transition = UnityEngine.UI.Selectable.Transition.ColorTint;
+                    UnityEngine.UI.ColorBlock state = button.colors;
+                    state.pressedColor = color;
+                    button.colors = state;
+                }
+                else if (lowerName.Contains("disabled"))
+                {
+                    button.transition = UnityEngine.UI.Selectable.Transition.ColorTint;
+                    UnityEngine.UI.ColorBlock state = button.colors;
+                    state.disabledColor = color;
+                    button.colors = state;
+                }
+                else if (lowerName.Contains("highlighted"))
+                {
+                    button.transition = UnityEngine.UI.Selectable.Transition.ColorTint;
+                    UnityEngine.UI.ColorBlock state = button.colors;
+                    state.highlightedColor = color;
+                    button.colors = state;
+                }
+            }
         }
     }
 }
