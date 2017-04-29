367535bc4c6cf4ce5c949b8bd7c6734e5efd6e5a
zouhangte <zouhangtezbm@126.com>
2017-04-29 13:23:47 +0800

    AdjustImage

--- PSUI/Editor/ImageImports/TextImport.cs
+++ PSUI/Editor/ImageImports/TextImport.cs
@@ -18,15 +18,27 @@ namespace PSDUIImporter
             float size;
             if (float.TryParse(image.arguments[2], out size))
             {
-                myText.fontSize = (int)size;
+                myText.fontSize = Mathf.CeilToInt(size);
             }
 
             myText.text = image.arguments[3];
 
             RectTransform rectTransform = myText.GetComponent<RectTransform>();
+            AdjustImage(image, myText.fontSize);
             rectTransform.sizeDelta = new Vector2(image.size.width, image.size.height);
             rectTransform.anchoredPosition = new Vector2(image.position.x, image.position.y);
             return node;
+        }
+        /// <summary>
+        /// 调节字边距
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="fontSize"></param>
+        /// <returns></returns>
+        private void AdjustImage(Image image,int fontSize)
+        {
+            image.size.width += fontSize;
+            image.size.height += fontSize * 0.5f;
         }
     }
 }
