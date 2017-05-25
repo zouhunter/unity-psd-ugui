d470ebd378e7a64ee1f024626603ead1eae27767
zouhangte <zouhangtezbm@126.com>
2017-05-25 17:12:33 +0800

    更新Demo

---
+++ Assets/PSUI/Editor/ImageImports/TextImport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PSDUIImporter
+{
+    public class TextImport : IImageImport
+    {
+        public UINode DrawImage(Image image, UINode parent)
+        {
+            UINode node = PSDImportUtility.InstantiateItem(PSDImporterConst.PREFAB_PATH_TEXT, image.name,parent);
+            UnityEngine.UI.Text myText = node.InitComponent<Text>();
+            PSDImportUtility.SetPictureOrLoadColor(image, myText);
+
+            float size;
+            if (float.TryParse(image.arguments[2], out size))
+            {
+                myText.fontSize = Mathf.CeilToInt(size);
+            }
+
+            myText.text = image.arguments[3];
+
+            RectTransform rectTransform = myText.GetComponent<RectTransform>();
+            AdjustImage(image, myText.fontSize);
+            rectTransform.sizeDelta = new Vector2(image.size.width, image.size.height);
+            rectTransform.anchoredPosition = new Vector2(image.position.x, image.position.y);
+            return node;
+        }
+        /// <summary>
+        /// 调节字边距
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="fontSize"></param>
+        /// <returns></returns>
+        private void AdjustImage(Image image,int fontSize)
+        {
+            image.size.width += 40;
+            image.size.height += 20;
+        }
+    }
+}
---
+++ Assets/PSUI/Editor/LayerImport/ScrollViewLayerImport.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PSDUIImporter
+{
+    public class ScrollViewLayerImport : ILayerImport
+    {
+        PSDImportCtrl ctrl;
+        public ScrollViewLayerImport(PSDImportCtrl ctrl)
+        {
+            this.ctrl = ctrl;
+        }
+        public UINode DrawLayer(Layer layer, UINode parent)
+        {
+            UINode node = PSDImportUtility.InstantiateItem(PSDImporterConst.PREFAB_PATH_SCROLLVIEW,layer.name,parent);
+            UnityEngine.UI.ScrollRect scrollRect = node.InitComponent<UnityEngine.UI.ScrollRect>();
+
+            UINode childNode = PSDImportUtility.InstantiateItem(PSDImporterConst.PREFAB_PATH_IMAGE, "Viewport", node);
+            scrollRect.viewport = childNode.InitComponent<RectTransform>();
+            Color color;
+            if (ColorUtility.TryParseHtmlString("#FFFFFF01",out color))
+            {
+                childNode.InitComponent<UnityEngine.UI.Image>().color = color;
+                Debug.Log(color);
+            }
+            childNode.InitComponent<Mask>();
+            childNode.anchoType = UINode.AnchoType.XStretch | UINode.AnchoType.YStretch;
+
+            bool havebg = false;
+            for (int i = 0; i < layer.images.Length; i++)
+            {
+                Image image = layer.images[i];
+
+                if (image.name.ToLower().StartsWith("b_"))
+                {
+                    havebg = true;
+                    UnityEngine.UI.Image graph = node.InitComponent<UnityEngine.UI.Image>();
+                    Debug.Log(graph);
+                    Debug.Log(image);
+
+                    PSDImportUtility.SetPictureOrLoadColor(image, graph);
+
+                    PSDImportUtility.SetRectTransform(image, scrollRect.GetComponent<RectTransform>());
+                }
+                else
+                {
+                    ctrl.DrawImage(image, node);
+                }
+            }
+
+            if (!havebg)
+            {
+                PSDImportUtility.SetRectTransform(layer, scrollRect.GetComponent<RectTransform>(),parent.InitComponent<RectTransform>());
+            }
+
+            PSDImportUtility.SetRectTransform(layer, childNode.InitComponent<RectTransform>(), scrollRect.GetComponent<RectTransform>());
+
+            if (layer.arguments != null)
+            {
+                string type = layer.arguments[0].ToUpper();
+                switch (type)
+                {
+                    case "V":
+                        scrollRect.vertical = true;
+                        scrollRect.horizontal = false;
+                        break;
+                    case "H":
+                        scrollRect.vertical = false;
+                        scrollRect.horizontal = true;
+                        break;
+                    case "VH":
+                    case "HV":
+                        scrollRect.vertical = true;
+                        scrollRect.horizontal = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (layer.layers != null)
+            {
+                for (int i = 0; i < layer.layers.Length; i++)
+                {
+                    Layer child = layer.layers[i];
+                    string childLowerName = child.name;
+                    UINode c_Node = ctrl.DrawLayer(child, childNode);
+
+                    if (childLowerName.StartsWith("c_"))
+                    {
+                        scrollRect.content = c_Node.InitComponent<RectTransform>();
+                    }
+                    else if (childLowerName.StartsWith("vb_"))
+                    {
+                        scrollRect.verticalScrollbar = c_Node.InitComponent<Scrollbar>();
+                        scrollRect.verticalScrollbarVisibility = ScrollRect.ScrollbarVisibility.AutoHide;
+                    }
+                    else if (childLowerName.StartsWith("hb_"))
+                    {
+                        scrollRect.horizontalScrollbar = c_Node.InitComponent<Scrollbar>();
+                        scrollRect.horizontalScrollbarVisibility = ScrollRect.ScrollbarVisibility.AutoHide;
+                    }
+                }
+            }
+            return node;
+        }
+    }
+}
