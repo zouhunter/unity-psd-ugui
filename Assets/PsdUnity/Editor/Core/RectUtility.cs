678ed1eaaca7b015db7ba850495c6da0597996f8
zouhangte <zouhangtezbm@126.com>
2017-06-06 19:16:10 +0800

    move *.psd outside of Assets

---
+++ Assets/PsdUnity/Editor/Core/RectUtility.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections.Generic;
+using System.Drawing;
+
+public class RectUtility {
+    public static Rect GetRectByRectRange(Rectangle rect0)
+    {
+        return new Rect(rect0.X, rect0.Height - rect0.Y, rect0.Width, rect0.Height);
+    }
+}
