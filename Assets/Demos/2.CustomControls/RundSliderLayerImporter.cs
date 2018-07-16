5c4db9b6e4dcd6b4bd9ad02b3e9e1e88e58d7361
zouhunter <1063627025@qq.com>
2018-07-16 20:50:07 +0800

    测试自定义控件

---
+++ Assets/Demos/2.CustomControls/RundSliderLayerImporter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using PSDUnity.UGUI;
+using System;
+
+public class RundSliderLayerImporter : LayerImport
+{
+    public RundSliderLayerImporter()
+    {
+        _suffix = "RoundSlider";
+    }
+
+    public override GameObject CreateTemplate()
+    {
+        return new GameObject("RoundSlider", typeof(Image));
+    }
+}
